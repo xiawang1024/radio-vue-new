7455809f6164a9d798634291ef30fef0d11fdf22
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-31 16:26:04 +0800

    播放器底部组件

---
+++ src/components/playerFooter/playerFooter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="player-footer">
+        <div class="qrcode"></div>
+        <div class="logo"></div>
+    </div> 
+</template>
+
+<script>
+export default {
+  name:'player-footer'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../common/stylus/variable.styl'
+.player-footer
+    width 100%
+    height 460px
+    background $color-background
+    .qrcode
+        margin-top 60px
+        width 100%
+        height 170px
+        background url('./qrcode.png') center center no-repeat
+        background-size 170px auto
+    .logo
+        margin-top 50px
+        width 100%
+        height 100px
+        background url('./logo.png') center center no-repeat
+        background-size auto 100px
+</style>
--- src/pages/player/player.vue
+++ src/pages/player/player.vue
@@ -3,17 +3,20 @@
         <audio src="http://lhttp.qingting.fm/live/5022452/64k.mp3" id="audio" autoplay style="display:none"></audio>
         <player-header></player-header>
         <player-core></player-core>
+        <player-footer></player-footer>
     </div>
 </template>
 
 <script>
 import PlayerHeader from 'components/playerHeader/playerHeader'
 import PlayerCore from 'components/playerCore/playerCore'
+import PlayerFooter from 'components/playerFooter/playerFooter'
 export default {
     name:'player',
     components:{
         PlayerHeader,
-        PlayerCore
+        PlayerCore,
+        PlayerFooter
     }
 }
 </script>
