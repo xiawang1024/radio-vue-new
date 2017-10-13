eb1a289013ff58ae15c26457d125d417e4001507
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-10-13 10:53:17 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,41 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'pages/home/home'
 import Player from 'pages/player/player'
+import Fmpage from 'pages/fmpage/fmpage'
+import List from 'pages/list/list'
+import Inner from 'pages/inner/inner'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path:'/',
-      redirect:'/home'
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/player',
-      name: 'Player',
-      component: Player
-    }
-  ]
+    routes: [{
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/player',
+            name: 'player',
+            component: Player
+        },
+        {
+            path: '/fmpage',
+            name: 'fmpage',
+            component: Fmpage
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: List
+        },
+        {
+            path: '/inner',
+            name: 'inner',
+            component: Inner
+        }
+    ]
 })
